a5b0c3d76178c57803f15ae5f508054f22bd60b7
Marc Müller <marc.mueller04@gmail.com>
2023-07-06 21:02:57 +0200

    example post request

--- src/app/api/recipes/route.ts
+++ src/app/api/recipes/route.ts
@@ -3,6 +3,23 @@ import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export async function GET() {
-  const res = await prisma.benutzer.findMany();
-  return NextResponse.json(res);
+  const data = await prisma.benutzer.findMany();
+  return NextResponse.json(data);
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, surname, email, password, telephone, gender } = body;
+  const benutzer = await prisma.benutzer.create({
+    data: {
+      name,
+      surname,
+      email,
+      password,
+      telephone,
+      gender,
+    },
+  });
+
+  return NextResponse.json(benutzer);
 }
