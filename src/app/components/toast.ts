0169f45dbb3355f0c065be8015cdda9732ca617b
Marc Müller <marc.mueller04@gmail.com>
2023-07-21 22:54:54 +0200

    del toast

--- src/app/components/toast.ts
+++
@@ -1,9 +0,0 @@
-import { toast } from "react-toastify";
-
-export default function toastHandler() {
-  toast("Login successful!", {
-    theme: "light",
-    type: "success",
-    autoClose: 1000,
-  });
-}
