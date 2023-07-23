973b60879bad106be2ea0a47adfd17d95156a572
Marc Müller <marc.mueller04@gmail.com>
2023-07-23 21:19:37 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        source: "/src/app/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
