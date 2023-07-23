efd38c5b4e050a54d27b9058dd4e8590a57bc9af
Marc Müller <marc.mueller04@gmail.com>
2023-07-23 21:35:17 +0200

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: Request) {
+  const origin = request.headers.get("origin");
+  const allowedOrigins = "*";
+  console.log("origin", origin);
+  if (origin && !allowedOrigins.includes(origin)) {
+    return new NextResponse(null, {
+      status: 400,
+      statusText: "BadRequest",
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+  return NextResponse.next();
+}
+export const config = {
+  matcher: "/about/:path*",
+};
--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/src/app/api/:path*",
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
