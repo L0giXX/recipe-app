1d28596583419abe3e01901c7ff69c5beabd7bcc
Marc Müller <marc.mueller04@gmail.com>
2023-07-26 17:12:41 +0200

    github provider

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,14 @@
 import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GitHubProvider from "next-auth/providers/github";
 import { prisma } from "../../../../utils/prisma";
 
 export const options: NextAuthOptions = {
   providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
--- src/utils/env.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-const envSchema = z.object({
-  JWT_SECRET: z.string().nonempty(),
-});
-
-export const env = envSchema.parse(process.env);
