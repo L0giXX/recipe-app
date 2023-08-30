df6b7312124bcc7ece314b6c9b3101fb1b342efe
L0giXX <marc.mueller04@gmail.com>
2023-08-30 16:51:53 +0200

    text reveal animation home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,17 +13,43 @@ type User =
 export default async function Home() {
   const session = await getServerSession(options);
   const user = session?.user as User;
+
+  const TextReveal = () => {
+    if (session) {
+      const text = `Hello ${user?.name}, and welcome to our recipe website`;
+      return (
+        <h1 className="mx-2 overflow-hidden text-4xl font-bold text-gray-900">
+          {text.match(/./gu)!.map((char, index) => (
+            <span
+              className="inline-block animate-text-reveal [animation-fill-mode:backwards]"
+              key={`${char}-${index}`}
+              style={{ animationDelay: `${index * 0.05}s` }}
+            >
+              {char === " " ? "\u00A0" : char}
+            </span>
+          ))}
+        </h1>
+      );
+    }
+    const text = "Hello, and welcome to our recipe website";
+    return (
+      <h1 className="overflow-hidden text-2xl font-bold leading-6 text-gray-900">
+        {text.match(/./gu)!.map((char, index) => (
+          <span
+            className="inline-block animate-text-reveal [animation-fill-mode:backwards]"
+            key={`${char}-${index}`}
+            style={{ animationDelay: `${index * 0.05}s` }}
+          >
+            {char === " " ? "\u00A0" : char}
+          </span>
+        ))}
+      </h1>
+    );
+  };
+
   return (
-    <main className="mx-5 mb-64 mt-64 flex flex-wrap justify-center text-6xl font-bold text-gray-900">
-      <div className="">
-        {session ? (
-          <div className="">
-            Hello {user?.name}, and welcome to our recipe website
-          </div>
-        ) : (
-          <div>Hello Guest, and welcome to our recipe website</div>
-        )}
-      </div>
+    <main className="my-52 flex justify-center">
+      <TextReveal />
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       display: ["group-hover"],
+      animation: {
+        "text-reveal": "text-reveal 1.5s cubic-bezier(0.77, 0, 0.175, 1) 0.5s",
+      },
+      keyframes: {
+        "text-reveal": {
+          "0%": {
+            transform: "translate(0, 100%)",
+          },
+          "100%": {
+            transform: "translate(0, 0)",
+          },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
